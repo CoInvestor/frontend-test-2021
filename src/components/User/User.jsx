14722f2f5b61e0ae5089151b6b124d49c7a3c6d2
zak aden <faf>
2021-10-15 20:57:42 +0100

    search functionality added for User Component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -3,10 +3,28 @@ import styles from "./User.module.scss";
 import { Link } from "react-router-dom";
 
 const User = ({ data }) => {
+  const [inputName, setInputName] = useState("");
+
+  // filters data through user search input and returns desired data
+  const filteredSearch = data.filter((person) => {
+    return person.name
+      .toLowerCase()
+      .includes(inputName.toLocaleLowerCase().trim());
+  });
+
   return (
     <div className={styles.container}>
+      <div className={styles.container__search}>
+        <h3>Search Name</h3>
+        <input
+          type="text"
+          value={inputName}
+          onChange={(e) => setInputName(e.target.value)}
+        />
+      </div>
+
       <div className={styles.container__user}>
-        {data.map((person) => (
+        {filteredSearch.map((person) => (
           <Link className={styles.button} to={`/${person.id}`}>
             <h4>{person.name}</h4>
           </Link>
