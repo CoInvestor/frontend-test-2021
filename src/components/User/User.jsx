ac0841a7e465062e74ad2c1a087be900e77b802e
zak aden <faf>
2021-10-15 20:50:35 +0100

    react router added for specific profile/data

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import styles from "./User.module.scss";
+import { Link } from "react-router-dom";
 
 const User = ({ data }) => {
   return (
     <div className={styles.container}>
       <div className={styles.container__user}>
         {data.map((person) => (
-          <a href="">
+          <Link className={styles.button} to={`/${person.id}`}>
             <h4>{person.name}</h4>
-          </a>
+          </Link>
         ))}
       </div>
     </div>
