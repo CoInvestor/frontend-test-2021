7422794f5bba3309347aad040483101826798f1c
zak aden <faf>
2021-10-15 21:33:32 +0100

    readMe updated

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
 # Frontend developer test
 
-Run `yarn install` to install all dependencies
-you will find all instructions for this task within the homepage when you first run `yarn start`
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+The test took me roughly 1 hour to complete. I created a custom axios/fetch hook so i could use repeatable code to fetch data without repeating it. The page is fully responsive. I've also added a search function so the user can filter through the data. You can click any card which directs you to a page with more information about user.
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -7,39 +7,38 @@ const Home = () => {
  <div className="home">
    <div className="container">
     <div className="task">
-     <h1>Frontend developer test</h1>
+     <h1>Zak's Frontend developer test</h1>
 
     <strong>Estimated time</strong>
-    <p>We expect the requirements to take around 1 - 2 hours to complete but feel free to have fun with it!</p>
+    <p>The test took me about 1 hour (plus 15 minutes of procrasination)</p>
+    <p>I didn't know what to do with this page so i decided to put extra information on how i found the test</p>
 
     <strong>Background info</strong>
-    <p>We are using <a href="https://jsonplaceholder.typicode.com/">JSON Placeholder</a> to grab test data for this so it may be worth having a read about what api endpoints are available.</p>
-    <p>We are using <a href="https://axios-http.com/">Axios</a> to do the base API request found inside views/Users</p>
+    <p>I created a custom fetch/axios hook called useAxios.js in src folder so i can reuse the fetch code without having to type the same block of code again</p>
+    
     
     <strong>Task</strong>
-
-      <p>Inside views/Users you will find a request to get an array of objects for users. We currently just console log out the data we get back. Use this data to output individual User components in a styled list / grid</p>
-      <p>A user component should contain a user's basic details with a link through to their profile. We'll leave this down to your discretion on style.</p>
-      <p>As stated above, each user component should link through to a profile page. This profile will also need to be created and hooked up using React Router logic so that they are dynamic routes for each user. This should show additional information for that specific selected user.</p>
+      <p>Page is fully responsive. </p>
+      <p>A Search is added so the user can filter through the data via searches</p>
 
 
     <strong>Requirements</strong>
     <ul>
-      <li>Needs to be responsive</li>
-      <li>Code should be commented where it makes sense to do so or should include a readMe</li>
-      <li>Follow best practices</li>
+      <li>Needs to be responsive ✓</li>
+      <li>Code should be commented where it makes sense to do so or should include a readMe ✓</li>
+      <li>Follow best practices ✓</li>
     </ul>
 
     <strong>Bonus</strong>
     <ul>
       <li>
-        Add back button from user profile
+        Add back button from user profile ✓
       </li>
       <li>
-        Add simple search functionality to list of users
+        Add simple search functionality to list of users ✓
       </li>
       <li>
-        Improve the original code / directory structure
+        Improve the original code / directory structure (subject to interpretation but i think ✓)
       </li>
     </ul>
     </div>
