7e97ada990578300ebbd95dc5730b04af618aefb
zak aden <faf>
2021-10-15 20:59:32 +0100

    custom hook used in Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useParams } from "react-router";
+import useAxios from "../../useAxios";
+import styles from "./Profile.module.scss";
 
 const Profile = () => {
+  const { id } = useParams();
+
+  // custom axios hook
+  const { data, error, isLoading } = useAxios(
+    "https://jsonplaceholder.typicode.com/users/" + id
+  );
+
+  console.log(data);
   return (
     <div>
       <p>Profile</p>
